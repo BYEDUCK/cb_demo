212f82ca608fa2b6d022aa835b798e8486313ee3
Mateusz Bajdak <m.bajdak@samsung.com>
2019-04-10 17:09:06 +0200

    fix countBreweriesByCountry

--- src/main/kotlin/com/example/cbDemo/repository/BreweryRepository.kt
+++ src/main/kotlin/com/example/cbDemo/repository/BreweryRepository.kt
@@ -1,12 +1,14 @@
 package com.example.cbDemo.repository
 
 import com.example.cbDemo.domain.Brewery
+import org.springframework.data.couchbase.core.query.View
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface BreweryRepository : CrudRepository<Brewery, String> {
 
+    @View(designDocument = "brewery", viewName = "by_country", reduce = true)
     fun countBreweriesByCountry(country: String?): Int
 
 }
